8b0069f8d33f753589281aea77717008d105239f
Axl47 <axelorta2153@gmail.com>
2021-10-28 20:06:15 -0400

    Pause, Resume, Stop and Loop implemented

--- commands/play.js
+++ commands/play.js
@@ -7,10 +7,11 @@ const events = music.event;
 
 module.exports = {
     name: 'play',
-    aliases: ['p', 'skip', 's', 'pause', 'leave'],
+    aliases: ['p', 'skip', 's', 'pause', 'resume', 'stop', 'loop'],
     description: 'Music bot',
     async execute(message, args, cmd, client, Discord){
         const channel = message.member.voice.channel;
+
         if(!channel) return message.channel.send(`You need to be in a channel to execute this command ${message.author}!`);
         if(cmd === 'play' || cmd === 'p'){
             if(!args.length) return message.channel.send(`I don't know what to search for ${message.author}!`);
@@ -42,17 +43,26 @@ module.exports = {
             const queue = await music.getQueue({interaction: message});
             if(queue.length > 1) return message.channel.send(`:thumbsup: Added **${songs.title}** to the queue!`);
             else return message.channel.send(`:musical_note:  Started playing **${songs.title}**!`);
-            //message.channel.send(`:musical_note:  Started playing the song **${songs.title}**!`);
         } else if(cmd === 'skip' || cmd === 's'){
             music.skip({ interaction: message });
             message.reply('Song skipped.');
             return message.channel.send(`:musical_note: Started playing **${songs.title}**!`);
+        } else if(cmd === 'pause'){
+            music.pause({ interaction: message });
+            message.reply('Playback paused.');
+        } else if(cmd === 'resume'){
+            music.resume({ interaction: message });
+            message.reply('Playback resumed.');
+        } else if(cmd === 'stop'){
+            music.stop({ interaction: message });
+            message.reply('Playback stopped.');
+        } else if(cmd === 'loop'){
+            const loop = await music.isRepeated({interaction: message});
+            music.repeat({
+                interaction: message,
+                value: !loop
+            });
+            return (!loop) ? message.reply('Now looping.') : message.reply('Stopped looping.')
         }
-        //music.event.on('playSong', async () => {
-            //messagePlay(message, songs);
-        //});
-        //music.event.on('addSong', async () => {
-            //messageAdd(message, songs);
-        //});
     }
 }
