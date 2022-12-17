d2e46729f34166963f5bacf4a5d31f9d2bd0a771
Axl47 <axelorta2153@gmail.com>
2022-12-17 19:36:02 -0400

    Removed loops from jump command

--- src/commands/music/jump.ts
+++ src/commands/music/jump.ts
@@ -34,23 +34,7 @@ export default new Command({
 			const amount = args.getInteger('amount', true);
 
 			if (songQueue.loop !== LoopState.Disabled) {
-				// TODO: A % operation might delete this while loop
-
-				// If we are looping, and the amount to skip is greater than
-				// the length of the song, loop through the queue to find the song
-				if (songQueue.songs.length < amount + songQueue.loopCounter) {
-					let number = amount;
-					while (number) {
-						number--;
-						songQueue.loopCounter++;
-						if (songQueue.loopCounter >= songQueue.songs.length) {
-							songQueue.loopCounter = 0;
-						}
-					}
-				}
-				else {
-					songQueue.loopCounter += amount;
-				}
+				songQueue.loopCounter = amount % songQueue.songs.length;
 			}
 			else if (songQueue.songs.length < amount) {
 				response
@@ -58,11 +42,9 @@ export default new Command({
 					.setDescription(`Queue length is ${songQueue.songs.length}.`);
 			}
 			else {
-				// TODO: array.slice() can delete this for loop
-				for (let i = 0; i < amount; i++) {
-					songQueue.songs.shift();
-				}
+				songQueue.songs.slice(amount - 1);
 			}
+
 			await videoPlayer(
 				interaction.commandGuildId!,
 				songQueue.songs[songQueue.loopCounter],
